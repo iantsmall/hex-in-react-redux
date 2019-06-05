80c4bb57cc49adca5f11f219243c0b44cd227746
Ian T. Small <hello@iantsmall.com>
2019-06-05 22:16:46 +0000

    Removed old 'utils'

--- app/containers/oldapp/hex-game.js
+++ app/containers/oldapp/hex-game.js
@@ -11,7 +11,22 @@ import {
   Col,
 } from 'react-bootstrap';
 import HexBoard from './hex-board';
-import { convertArrayToMatrix } from './utils';
+
+const convertArrayToMatrix = (arr, width) => {
+  const rows = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    const nextElement = arr[index];
+    const isFirstRowElement = index % width === 0;
+    if (isFirstRowElement) {
+      // make a new row from first element
+      rows.push([nextElement]);
+    } else {
+      // push onto current row
+      rows[rows.length - 1].push(nextElement);
+    }
+  }
+  return rows;
+};
 
 const getNeighbors = (hex, matrix) => {
   const getHex = (x, y) => (matrix[y] !== undefined ? matrix[y][x] : undefined);
