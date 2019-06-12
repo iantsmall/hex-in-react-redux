e790fcd6c389831e57a157dd16d953ac6c31a904
IANS-SURFACE1\iansm <ian.small@thimble.io>
2019-06-12 10:32:06 -0400

    Added Menu.actions tests

--- app/containers/Menu/tests/actions.test.js
+++ app/containers/Menu/tests/actions.test.js
@@ -1,14 +1,47 @@
-// import { defaultAction } from '../actions';
-// import { DEFAULT_ACTION } from '../constants';
+import {
+  showRulesAction,
+  showComingSoonAction,
+  hideRulesAction,
+  hideComingSoonAction,
+} from '../actions';
+import {
+  SHOW_RULES_ACTION,
+  SHOW_COMING_SOON_ACTION,
+  HIDE_RULES_ACTION,
+  HIDE_COMING_SOON_ACTION,
+} from '../constants';
 
 describe('Menu actions', () => {
-  // TODO define action tests
-  // describe('Default Action', () => {
-  //   it('has a type of DEFAULT_ACTION', () => {
-  //     const expected = {
-  //       type: DEFAULT_ACTION,
-  //     };
-  //     expect(defaultAction()).toEqual(expected);
-  //   });
-  // });
+  describe('Show Rules Action', () => {
+    it('has a type of SHOW_RULES_ACTION', () => {
+      const expected = {
+        type: SHOW_RULES_ACTION,
+      };
+      expect(showRulesAction()).toEqual(expected);
+    });
+  });
+  describe('Show Coming Soon Action', () => {
+    it('has a type of SHOW_COMING_SOON_ACTION', () => {
+      const expected = {
+        type: SHOW_COMING_SOON_ACTION,
+      };
+      expect(showComingSoonAction()).toEqual(expected);
+    });
+  });
+  describe('Hide Rules Action', () => {
+    it('has a type of HIDE_RULES_ACTION', () => {
+      const expected = {
+        type: HIDE_RULES_ACTION,
+      };
+      expect(hideRulesAction()).toEqual(expected);
+    });
+  });
+  describe('Hide Coming Soon Action', () => {
+    it('has a type of HIDE_COMING_SOON_ACTION', () => {
+      const expected = {
+        type: HIDE_COMING_SOON_ACTION,
+      };
+      expect(hideComingSoonAction()).toEqual(expected);
+    });
+  });
 });
