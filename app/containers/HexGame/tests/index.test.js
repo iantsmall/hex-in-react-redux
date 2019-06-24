0b6cb70330375ac35dacd8f11d77dd1bb1f1bb55
IANS-SURFACE1\iansm <ian.small@thimble.io>
2019-06-24 13:14:53 -0400

    Added skeleton cases for HexGame

--- app/containers/HexGame/tests/index.test.js
+++ app/containers/HexGame/tests/index.test.js
@@ -22,7 +22,51 @@ describe('<HexGame />', () => {
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it.skip('Expect to have additional unit tests specified', () => {
+  it.skip('Should dispatch a jumpToFirstAction when First Turn button is clicked', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
+    expect(true).toEqual(false);
+  });
+
+  it.skip('Should dispatch a jumpToPreviousAction when Previous Turn button is clicked', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
+    expect(true).toEqual(false);
+  });
+
+  it.skip('Should dispatch a jumpToNextAction when Previous Turn button is clicked', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
+    expect(true).toEqual(false);
+  });
+
+  it.skip('Should dispatch a jumpToLastAction when Last Turn button is clicked', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
+    expect(true).toEqual(false);
+  });
+
+  it.skip('Should dispatch a jumpToAction when a Turn is selected', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
+    expect(true).toEqual(false);
+  });
+
+  it.skip('Should dispatch a claimHexAction when a Hex is clicked', () => {
+    const dispatch = jest.fn();
+    render(
+      <HexGame dispatch={dispatch} boardSize={9} hexGame={{ boardSize: 5 }} />,
+    );
     expect(true).toEqual(false);
   });
 
@@ -31,7 +75,7 @@ describe('<HexGame />', () => {
    *
    * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
    */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const dispatch = jest.fn();
     const {
       container: { firstChild },
